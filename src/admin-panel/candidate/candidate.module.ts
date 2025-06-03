7985e6d09dd5e741906905698319f3a7eb216fdb
gustavokurtz <kurtzff32@gmail.com>
2025-06-03 17:59:51 -0300

    feat: criando dto de candidato

--- src/admin-panel/candidate/candidate.module.ts
+++ src/admin-panel/candidate/candidate.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CandidateService } from './candidate.service';
 import { CandidateController } from './candidate.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [CandidateController],
   providers: [CandidateService],
 })
--- src/admin-panel/candidate/candidate.service.ts
+++ src/admin-panel/candidate/candidate.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class CandidateService {}
+export class CandidateService {
+    constructor(prisma: PrismaService) {
+        // Initialization logic can go here if needed
+    }
+
+    // Define methods for candidate-related operations here
+    
+}
