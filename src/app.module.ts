da6fb85adbe0c2639f358adae65443077dbc3495
gustavokurtz <kurtzff32@gmail.com>
2025-06-03 17:56:21 -0300

    feat: adicionando componente de candidatos

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { JobModule } from './admin-panel/jobs/job.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { JobapplicationModule } from './admin-panel/jobapplication/jobapplication.module';
+import { CandidateModule } from './admin-panel/candidate/candidate.module';
 
 
 @Module({
-  imports: [JobModule, PrismaModule, JobapplicationModule],
+  imports: [JobModule, PrismaModule, JobapplicationModule, CandidateModule],
   controllers: [AppController],
   providers: [AppService],
 })
